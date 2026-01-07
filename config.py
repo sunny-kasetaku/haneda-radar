e555cb57e8ffb9cdfa4df2651be494780792802b
sunny-kasetaku <huckn37@gmail.com>
2026-01-08 04:19:44 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ CONFIG = {
     "DATA_FILE": "haneda_raw.html",
     "RESULT_FILE": "analysis_result.json",
     "RESULT_JSON": "analysis_result.json",
-    "REPORT_FILE": "index.html",
-    "WAIT_SELECTOR": ".btn-flight-route" # 👈 便名が表示されるボタンが出るまで待機
+    "REPORT_FILE": "index.html"
 }
