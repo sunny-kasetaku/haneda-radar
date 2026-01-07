4c3f663b51f9082bf70ab4e6a82d76a46b2a8652
sunny-kasetaku <huckn37@gmail.com>
2026-01-08 04:00:42 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 CONFIG = {
-    # ターゲット：Flightradar24 羽田到着便
     "TARGET_URL": "https://www.flightradar24.com/data/airports/hnd/arrivals",
-    
-    # 取得した生データの保存先
     "DATA_FILE": "haneda_raw.html",
-    
-    # 解析結果の保存先 (ここが足りていませんでした)
     "RESULT_FILE": "analysis_result.json",
-    
-    # HTMLレポートの出力先
+    "RESULT_JSON": "analysis_result.json", # renderer.pyが欲しがっている名前を追加
     "REPORT_FILE": "index.html"
 }
