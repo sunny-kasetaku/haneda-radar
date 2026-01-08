819e18dac0ecf1657dc9d9ae31e8af7f7ef0479d
sunny-kasetaku <huckn37@gmail.com>
2026-01-08 17:04:16 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+# ==========================================
+# Project: KASETACK - config.py
+# ==========================================
 CONFIG = {
+    "API_KEY": "76e04028a66e0e2d2b42d7d9c75462e7", # プロデューサー、ここにキーを入力してください
     "TARGET_URL": "https://www.flightradar24.com/data/airports/hnd/arrivals",
     "DATA_FILE": "haneda_raw.json",
     "RESULT_FILE": "analysis_result.json",
     "RESULT_JSON": "analysis_result.json",
     "REPORT_FILE": "index.html",
-    "CACHE_LIMIT_SEC": 300  # API節約：5分間はキャッシュを利用
+    "CACHE_LIMIT_SEC": 300
 }
