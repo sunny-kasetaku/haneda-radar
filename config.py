ebcd8905b4ff7e8b2b49754339490fc6363db27a
sunny-kasetaku <huckn37@gmail.com>
2026-01-08 15:41:22 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 CONFIG = {
     "TARGET_URL": "https://www.flightradar24.com/data/airports/hnd/arrivals",
-    "DATA_FILE": "haneda_raw.html",
+    "DATA_FILE": "haneda_raw.json",
     "RESULT_FILE": "analysis_result.json",
     "RESULT_JSON": "analysis_result.json",
     "REPORT_FILE": "index.html",
-    "WAIT_SELECTOR": ".btn-flight-route"
+    "CACHE_LIMIT_SEC": 300  # API節約：5分間はキャッシュを利用
 }
