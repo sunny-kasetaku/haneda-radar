c04c41618832670e99a1e1bc5fa141fd91dcbb0d
sunny-kasetaku <huckn37@gmail.com>
2026-01-24 14:03:24 +0900

    Update analyzer_v2.py

--- analyzer_v2.py
+++ analyzer_v2.py
@@ -1,4 +1,3 @@
-# analyzer_v2.py (コードシェア排除・強化版)
 from datetime import datetime, timedelta
 
 def analyze_demand(flights):
@@ -36,7 +35,7 @@ def analyze_demand(flights):
         term = str(f.get('terminal', ''))
         
         pax = 150 # デフォルト
-        if '3' in term or 'I' in term or 'Intl' in term:
+        if any(x in term for x in ['3', 'I', 'Intl']):
             pax = 250
         elif any(x in airline for x in ["WINGS", "J-AIR", "HAC", "AMX", "ORC", "IBEX"]):
             pax = 50
@@ -46,36 +45,22 @@ def analyze_demand(flights):
         try:
             if 'T' in t_str:
                 flight_time = datetime.strptime(t_str[:16], "%Y-%m-%dT%H:%M")
-            else:
-                continue
+                
+                if range_start <= flight_time <= range_end:
+                    unique_flights.append(f)
+                    if '1' in term: pax_t1 += pax
+                    elif '2' in term: pax_t2 += pax
+                    else: pax_t3 += pax
 
-            # 集計
-            if range_start <= flight_time <= range_end:
-                unique_flights.append(f)
-                if '1' in term:
-                    pax_t1 += pax
-                elif '2' in term:
-                    pax_t2 += pax
-                else:
-                    pax_t3 += pax
+                diff_hours = (flight_time - now).total_seconds() / 3600
+                if 0 <= diff_hours < 1: forecast["h1"]["pax"] += pax
+                elif 1 <= diff_hours < 2: forecast["h2"]["pax"] += pax
+                elif 2 <= diff_hours < 3: forecast["h3"]["pax"] += pax
+        except: continue
 
-            # 予測
-            diff_hours = (flight_time - now).total_seconds() / 3600
-            if 0 <= diff_hours < 1: forecast["h1"]["pax"] += pax
-            elif 1 <= diff_hours < 2: forecast["h2"]["pax"] += pax
-            elif 2 <= diff_hours < 3: forecast["h3"]["pax"] += pax
-
-        except:
-            continue
-
-    # 最終集計 (T1を南北に分配)
     return {
-        "1号(T1南)": int(pax_t1 * 0.5),
-        "2号(T1北)": int(pax_t1 * 0.5),
-        "3号(T2)":   int(pax_t2 * 0.5),
-        "4号(T2)":   int(pax_t2 * 0.5),
-        "国際(T3)":  pax_t3,
-        "forecast": forecast,
-        "unique_count": len(unique_flights),
-        "flights": unique_flights
+        "1号(T1南)": int(pax_t1 * 0.5), "2号(T1北)": int(pax_t1 * 0.5),
+        "3号(T2)": int(pax_t2 * 0.5), "4号(T2)": int(pax_t2 * 0.5),
+        "国際(T3)": pax_t3, "forecast": forecast,
+        "unique_count": len(unique_flights), "flights": unique_flights
     }
